1cf1e3bf3121b81c77108ef05d421fe67431b42d
asierpl <150433843+asierpl@users.noreply.github.com>
2024-01-15 19:38:11 +0100

    tutoria

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,154 +1,156 @@
 
-// import { useEffect, useRef, useState } from "react"
-// import './Login.css'
+import { useEffect, useRef, useState } from "react"
+import './Login.css'
+import { useNavigate } from "react-router-dom"
 
-// export const Logino = () => {
+export const Acceso = () => {
 
-// const login = [
-//     {id: 0 , btn : "Iniciar sesion"},
-//     {id: 1 , btn : "Crear cuenta"}
-// ]
+const login = [
+    {id: 0 , btn : "Iniciar sesion"},
+    {id: 1 , btn : "Crear cuenta"}
+]
 
-//     const { VITE_URL_API } = import.meta.env
+    const { VITE_URL_API } = import.meta.env
 
+    const navigate = useNavigate()
+
+    const [ active , setActive ] = useState('')
+    const loginHandler = (valor) => setActive(valor)
    
-//     const [ active , setActive ] = useState('')
-//     const loginHandler = (valor) => setActive(valor)
-   
     
     
-//     const user = useRef('')
-//     const pass = useRef('')
+    const user = useRef('')
+    const pass = useRef('')
 
     
 
-//     const formHandler = (e) => {
-//         e.preventDefault()
+    const formHandler = (e) => {
+        e.preventDefault()
 
 
-//         let nuevo = {
-//             user : user.current.value,
-//             pass : pass.current.value,
-//         }
+        let nuevo = {
+            user : user.current.value,
+            pass : pass.current.value,
+        }
 
-//         let options = {
-//             method  : 'post' , 
-//             body    : JSON.stringify(nuevo),
-//             headers : {
-//                 "Content-type" : "application/json"
-//             }
-//         }
+        let options = {
+            method  : 'post' , 
+            body    : JSON.stringify(nuevo),
+            headers : {
+                "Content-type" : "application/json"
+            }
+        }
 
-//         fetch(VITE_URL_API , options)
-//         .then( res => res.json() )
-//         .then( data => {
+        fetch(VITE_URL_API , options)
+        .then( res => res.json() )
+        .then( data => {
             
-//             if( data ){
-//                 localStorage.setItem('usuarios' , JSON.stringify(data))
-//                 navigate('/gestor')
-//             }
-//         })
-//         .catch( error => console.log( error ))
-//     }
+            if( data ){
+                localStorage.setItem('usuarios' , JSON.stringify(data))
+                navigate('/gestor')
+            }
+        })
+        .catch( error => console.log( error ))
+    }
 
     
     
-//     return (
-//         <> 
-//         <div className="Container-login">
+    return (
+        <> 
+        <div className="Container-login">
 
-//             <img src="/assets/canon.png" alt="imagen" className="Canon-portada" />
+            <img src="/assets/canon.png" alt="imagen" className="Canon-portada" />
 
-//             <img className='Img-portada' src="/assets/equipo.png" alt="imagen" />
+            <img className='Img-portada' src="/assets/equipo.png" alt="imagen" />
 
-//             <div className="Open-login">
+            <div className="Open-login">
 
-//                { login.map( eachBtn => 
-//                <OpenLogin 
-//                key={eachBtn.id}
-//                {...eachBtn}
-//                loginHandler = {loginHandler}/>
-//             )}
+               { login.map( eachBtn => 
+               <OpenLogin 
+               key={eachBtn.id}
+               {...eachBtn}
+               loginHandler = {loginHandler}/>
+            )}
                
                 
-//             </div>
-//                 {(active === login[0].btn ) && (
-//             <div  className={`Login ${ active ? 'isActive' : '' } `}>
-//             <button 
-//                 className="Close-btn"
-//                 onClick={()=>loginHandler('')}>
-//                     <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-//                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
-//                     </svg>
-//             </button>
-//             <img className="Imagen-canon" src="/assets/canon.png" alt="imagen" />
+            </div>
+                {(active === login[0].btn ) && (
+            <div  className={`Login ${ active ? 'isActive' : '' } `}>
+            <button 
+                className="Close-btn"
+                onClick={()=>loginHandler('')}>
+                    <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg>
+            </button>
+            <img className="Imagen-canon" src="/assets/canon.png" alt="imagen" />
 
-//                 <form onSubmit={ formHandler }>
+                <form onSubmit={ formHandler }>
                     
-//                     <div className="Caja-login">
-//                         <label htmlFor="user">Nombre de usuario</label>
-//                     </div>
-//                         <input type="text"  className='Placeholder'  name="user" ref={user} placeholder="Ingresa tu nombre de usuario"/>
-//                     <div className="Caja-login">
-//                         <label htmlFor="pass">Contraseña</label>
-//                     </div>
-//                         <input type="password" className='Placeholder' name="pass" ref={pass} placeholder="Ingresa tu contraseña"/>
-//                     <div className="Caja-continuar">
-//                         <input type="submit"   value="Acceder" />
-//                     </div>
+                    <div className="Caja-login">
+                        <label htmlFor="user">Nombre de usuario</label>
+                    </div>
+                        <input type="text"  className='Placeholder'  name="user" ref={user} placeholder="Ingresa tu nombre de usuario"/>
+                    <div className="Caja-login">
+                        <label htmlFor="pass">Contraseña</label>
+                    </div>
+                        <input type="password" className='Placeholder' name="pass" ref={pass} placeholder="Ingresa tu contraseña"/>
+                    <div className="Caja-continuar">
+                        <input type="submit"   value="Acceder" />
+                    </div>
                     
-//                 </form>
+                </form>
                 
-//             </div>
-//             )}
+            </div>
+            )}
 
-//             {(active === login[1].btn ) && (
-//             <div className={`Crear ${ active ? 'isActive' : '' } `}>
-//             <button 
-//                 className="Close-btn"
-//                 onClick={()=>loginHandler('')}>
-//                     <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-//                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
-//                     </svg>
-//             </button>
-//             <img className="Imagen-canon" src="/assets/canon.png" alt="imagen" />
-//             <form>
+            {(active === login[1].btn ) && (
+            <div className={`Crear ${ active ? 'isActive' : '' } `}>
+            <button 
+                className="Close-btn"
+                onClick={()=>loginHandler('')}>
+                    <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg>
+            </button>
+            <img className="Imagen-canon" src="/assets/canon.png" alt="imagen" />
+            <form>
                 
-//                 <div className="Caja-iniciar">
-//                     <label htmlFor="user">Nombre de usuario</label>
-//                 </div>
-//                     <input type="text"  className='Placeholder'   name="user"  placeholder="Ingresa un nombre de usuario"/>
-//                 <div className="Caja-iniciar">
-//                     <label htmlFor="email">Correo electronico</label>
-//                 </div>
-//                     <input type="email"  className='Placeholder'  name="email" placeholder="Ingresa tu correo electronico o email"/>
-//                 <div className="Caja-iniciar">
-//                     <label htmlFor="pass">Contraseña</label>
-//                 </div>
-//                     <input type="password" className='Placeholder' name="pass"  placeholder="Ingresa una contraseña"/>
-//                 <div className="Caja-continuar">
-//                     <input type="submit"   value="Continuar" />
-//                 </div>
-//             </form>
-//                 </div>
-//                 )}
+                <div className="Caja-iniciar">
+                    <label htmlFor="user">Nombre de usuario</label>
+                </div>
+                    <input type="text"  className='Placeholder'   name="user"  placeholder="Ingresa un nombre de usuario"/>
+                <div className="Caja-iniciar">
+                    <label htmlFor="email">Correo electronico</label>
+                </div>
+                    <input type="email"  className='Placeholder'  name="email" placeholder="Ingresa tu correo electronico o email"/>
+                <div className="Caja-iniciar">
+                    <label htmlFor="pass">Contraseña</label>
+                </div>
+                    <input type="password" className='Placeholder' name="pass"  placeholder="Ingresa una contraseña"/>
+                <div className="Caja-continuar">
+                    <input type="submit"   value="Continuar" />
+                </div>
+            </form>
+                </div>
+                )}
 
-//         </div>
-//         </>
-//     )
-// }
+        </div>
+        </>
+    )
+}
 
 
-// const OpenLogin = (props) => {
-//     const {loginHandler , btn} = props
+const OpenLogin = (props) => {
+    const {loginHandler , btn} = props
 
-//     return(
-//         <>
-//         <button className="Open-btn" onClick={()=> loginHandler(btn)} >
-//             {btn}
-//         </button>
-//         </>
-//     )
-// }
+    return(
+        <>
+        <button className="Open-btn" onClick={()=> loginHandler(btn)} >
+            {btn}
+        </button>
+        </>
+    )
+}
 
 
