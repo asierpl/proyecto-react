549668124be8bb17997e228fc70b0113230e047a
asierpl <150433843+asierpl@users.noreply.github.com>
2024-01-15 19:43:15 +0100

    borrado page login.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@
 
 import { useNavigate } from 'react-router-dom'
 import { useEffect} from "react"
-import './Login.css'
 import { Acceso} from '../components/Login/Login'
 
 export const Login = () => {
